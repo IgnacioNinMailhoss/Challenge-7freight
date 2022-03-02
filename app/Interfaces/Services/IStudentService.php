3eadd7e69d3f5eebf3d7f6abe926ee490345a928
IgnacioNinMailhoss <hello@nearshore.io>
2022-03-02 18:25:08 +0600

    Delete IStudentService.php

--- app/Interfaces/Services/IStudentService.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Interfaces\Services;
-
-interface IStudentService
-{
-    public function canBookClass(int $studentId, int $classBlockId): bool;
-    public function canCancelClass(int $studentId, int $classBlockId): bool;
-}
